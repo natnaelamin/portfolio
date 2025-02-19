6d02de31a5b1e88e9983dcc608cc1b0088f9729b
Natnael Amin <natnaelamin24@gmail.com>
2025-02-19 20:58:25 +0300

    little update cv

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -20,7 +20,7 @@ const HeroSection = () => {
           <div>
             <button className='px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white mt-3'>
               <span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'>
-                <Link href="/nati_cv.pdf">Download CV</Link>
+                <Link href="/images/nati_cv.pdf">Download CV</Link>
               </span>
             </button>
           </div>
